f519e7a7a5b7b5a63ab9e2c831afbc3b8cff1dce
gamars99 <54536701+gamars99@users.noreply.github.com>
2020-10-08 16:59:33 +0200

    add list for legume in basket

--- myApp/src/app/panier/panier.page.ts
+++ myApp/src/app/panier/panier.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataProvider } from '../providers/data';
 
 @Component({
   selector: 'app-panier',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanierPage implements OnInit {
 
-  constructor() { }
+  constructor(private dataprovider: DataProvider) {
+    dataprovider.loadFromAPI();
+   }
 
   ngOnInit() {
   }
