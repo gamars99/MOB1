8015f7281994445e70db80635a9dc80fb08aef40
Oberson Gaël <54536800+Oberson-Gael@users.noreply.github.com>
2020-10-27 20:30:50 +0100

    add incremential basket and calcule total

--- myApp/src/app/panier/panier.page.ts
+++ myApp/src/app/panier/panier.page.ts
@@ -10,24 +10,53 @@ import { Storage } from '@ionic/storage';
 export class PanierPage implements OnInit {
 
 public basket = [];
+public total = 0;
 
   constructor(private dataprovider: DataProvider, private storage: Storage) {
     dataprovider.loadFromAPI();
    }
 
   ngOnInit() {
-    this.storage.get('basket').then(getToken => {
-      this.basket = getToken
-      console.log(this.basket);
-    })
+    //for total
+    this.storage.get('total').then(getTotal => {
+      //le local storage 'total' n'existe pas
+      if(!getTotal){
+        this.storage.set('total', this.total).then();
+      }
+    });
+
+    //for basket
+    this.storage.get('basket').then(getBasket => {
+      //le local storage 'basket' existe
+      if(getBasket){
+        this.basket = getBasket
+        this.basket.forEach(element => {
+          this.total += element.price;
+        });
+      //le local storage 'basket' n'existe pas (le crée)
+      }else{
+        
+        this.storage.set('basket', this.basket).then();
+      }      
+    });
+    
+    
   }
 
   addVegetableBasket($event){
     this.dataprovider.find($event.target.value).then((val) => {
       this.basket.push(val);
-      this.storage.set('basket', this.basket).then();
-      this.basket = this.basket;
+      this.total += val.price;
+
+      this.storage.set('basket', this.basket).then((val) =>{
+        this.basket = val;
+      });
+
     });
+  }
+
+  test(id){
+    console.log(id);
   }
 
 }
