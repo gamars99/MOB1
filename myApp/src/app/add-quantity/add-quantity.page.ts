832df5c5f7e6d4b624257e96f050e2a6daf49b66
gamars99 <54536701+gamars99@users.noreply.github.com>
2020-11-09 11:30:36 +0100

    finish time (API OK sur postman mais pas sur ionic) manque boucle sur les produits a modifier et les post (sur postman => ok)

--- myApp/src/app/add-quantity/add-quantity.page.ts
+++ myApp/src/app/add-quantity/add-quantity.page.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { ToastController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import * as _ from 'lodash'
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { $ } from 'protractor';
 import { count } from 'console';
 
@@ -18,13 +19,27 @@ export class AddQuantityPage implements OnInit {
   private receipt = [] ;
   public counter = [] ;
 
-  constructor(private router : Router, private dataprovider: DataProvider, private toaster: ToastController,private storage: Storage) { 
+  constructor(private router: Router,private dataprovider: DataProvider, private toaster: ToastController,private storage: Storage, private httpClient: HttpClient) { 
     dataprovider.loadFromAPI().then(getVegetable => {
       this.allVegetable = getVegetable;
+    });
+
+    dataprovider.loadUserFromAPI().then(val => {
+      if(val['user_type'] != 1){
+        //toast
+        this.toaster.create({
+          message: "Vous n'avez pas les droits !!!!",
+          duration: 2000,
+        }).then(toast => {
+          toast.present();
+        });
+        this.router.navigateByUrl("cagettes");
+      }
     });
   }
 
   ngOnInit() {
+
     this.storage.get('receipt').then(getReceipt => {
       if(!getReceipt){
         this.storage.set
@@ -33,7 +48,6 @@ export class AddQuantityPage implements OnInit {
   }
 
   validVegetable(id, newNomber){
-    console.log(newNomber)
     if(newNomber == null){
       //toast
       this.toaster.create({
@@ -45,7 +59,6 @@ export class AddQuantityPage implements OnInit {
     }else{
       this.dataprovider.find(id).then(val => {
         val['stock'] = newNomber;
-        // console.log(val['stock'])
         this.receipt.push(val);
 
         this.storage.set('receipt', this.receipt).then((val) => {
@@ -67,7 +80,37 @@ export class AddQuantityPage implements OnInit {
   }
 
   save(){
-    
+    let httpHeaders = new HttpHeaders({
+
+    });
+
+    let options = {
+      headers: httpHeaders
+    };
+    let postData = {
+      "quantities[0][id]": 1,
+      "quantities[0][quantity]": 50
+    }
+
+    this.httpClient.post('http://127.0.0.1:8000/api/products/stock/', postData, options)
+    .subscribe(data => {
+      console.log(data['_body'])
+      this.toaster.create({
+        message: "Modification OK ;)",
+        duration: 2000,
+      }).then(toas => {
+        toas.present();
+      });
+      this.restart();
+    }, error => {
+      console.log(error);
+      this.toaster.create({
+        message: "Erreur lors de la modificaiton",
+        duration: 2000,
+      }).then(toas => {
+        toas.present();
+      });
+    })
   }
 
   getAvailableVegetable(val){
